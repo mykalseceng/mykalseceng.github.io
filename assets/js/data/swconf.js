3b9518d805999a626b443085be8423e90b8a42af
mykalseceng <mykalseceng@users.noreply.github.com>
2026-05-12 05:45:13 +0000

    deploy: mykalseceng/dark-knight-blog@fec9041a872ac300079605054d65072b62539628

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1778564706',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/categories/', '/tags/', '/archives/', '/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
